1f264eeb7fa6dbd0a1107f4e97171ddaf304e0b2
Jerome Moores <30985199+Jeromemoores@users.noreply.github.com>
2022-05-07 21:19:07 -0500

    - IDEK MAN

--- client/src/Components/Home/Content/index.js
+++ client/src/Components/Home/Content/index.js
@@ -1,10 +1,69 @@
-import { TopNav } from '../../Navbar'
-
 export const PageContent = () => {
     return(
         <div>
-            <div>
-                <h1>Hello World</h1>
+            <div className='thing_wrapper'>
+                <div className='thing_left'>
+                    <h3>Our Shitty Principles</h3>
+                    <hr className='thing_hr'/>
+                    <p>
+                        blah blah blah stuff here idk what to put here i just used this from the template bs but made it from scratch because I am too lazy to try to copy the bull shit css that comes with the template.
+                        its not perfect but you get the idea
+                    </p>
+                </div>
+                <div className='thing_right'>
+                    <div className='thing_top'>
+                        <div className='thing_card1'>
+                            <h4>Title here</h4>
+                            <p>Thing here 2</p>
+                        </div>
+                        <div className='thing_card2'>
+                            <h4>Title here</h4>
+                            <p>Thing here 2</p>
+                        </div>
+                    </div>
+                    <div className='thing_bottom'>
+                        {/* <div className='thing_card3'>
+                            <h4>Title here</h4>
+                            <p>Thing here 2</p>
+                        </div> */}
+                        <div className='thing_card4'>
+                            <h4>Title here</h4>
+                            <p>Thing here 2</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className='thing_wrapper2'>
+                <div className='thing_left2'>
+                    <div className='thing_top'>
+                        <div className='thing_card1'>
+                            <h4>Title here</h4>
+                            <p>Thing here 2</p>
+                        </div>
+                        <div className='thing_card2'>
+                            <h4>Title here</h4>
+                            <p>Thing here 2</p>
+                        </div>
+                    </div>
+                    <div className='thing_bottom'>
+                        {/* <div className='thing_card3'>
+                            <h4>Title here</h4>
+                            <p>Thing here 2</p>
+                        </div> */}
+                        <div className='thing_card4'>
+                            <h4>Title here</h4>
+                            <p>Thing here 2</p>
+                        </div>
+                    </div>
+                </div>
+                <div className='thing_right2'>
+                    <h3>Our Shitty Principles</h3>
+                    <hr className='thing_hr'/>
+                    <p>
+                        blah blah blah stuff here idk what to put here i just used this from the template bs but made it from scratch because I am too lazy to try to copy the bull shit css that comes with the template.
+                        its not perfect but you get the idea
+                    </p>
+                </div>
             </div>
         </div>
     )
